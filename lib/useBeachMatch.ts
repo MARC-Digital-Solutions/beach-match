1c86450e004c2fe5cdda870616fc88f4fbe21231
MARC-Digital-Solutions <dberg.marcdigitalsolutions@gmail.com>
2025-07-23 10:54:31 -0400

    ok

--- lib/useBeachMatch.ts
+++ lib/useBeachMatch.ts
@@ -6,6 +6,10 @@ import { EngagementTracker } from './engagementTracker';
 import { PowerUpSystem } from './powerUpSystem';
 import { EventManager } from './eventManager';
 
+if (process.env.NODE_ENV === 'development') {
+  console.log('[DEBUG] useBeachMatch.ts file loaded');
+}
+
 export function useBeachMatch(gameStarted: boolean = false) {
   const [gameState, setGameState] = useState<BeachMatchState>({
     score: 0,
